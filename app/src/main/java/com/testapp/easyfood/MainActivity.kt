8e43033444e973f72cbe85cb33a3d728cfcb812e
Ichangai <ichangaigitundu@gmail.com>
2022-07-07 11:56:28 +0300

    Added navigation controller

--- app/src/main/java/com/testapp/easyfood/MainActivity.kt
+++ app/src/main/java/com/testapp/easyfood/MainActivity.kt
@@ -2,6 +2,8 @@ package com.testapp.easyfood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.btm_nav)
+        val navController = Navigation.findNavController(this, R.id.host_fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
+
     }
 }
